13a73cdfa9e54bcb789d3bf42f6c7813c3c902b3
Squiddy <wolaki96@gmail.com>
2022-11-09 18:16:09 -0500

    added update for category

--- ObjectRelationalMapping/Develop/routes/api/category-routes.js
+++ ObjectRelationalMapping/Develop/routes/api/category-routes.js
@@ -35,13 +35,27 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+// update a category by its `id` value
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body,
+      {where: {id: req.params.id}}
+    );
+  if (!categoryData) {
+    res.status(404).json({ message: "No category found with this ID! Please try again."})
+  return;
+  }
+  res.status(200).json();
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
+// delete a category by its `id` value
+
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  
 });
 
 module.exports = router;
