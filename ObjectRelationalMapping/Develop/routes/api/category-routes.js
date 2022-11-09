bcb2ac136f0ee68ba59c439d9fee1a44858ec07e
Squiddy <wolaki96@gmail.com>
2022-11-09 18:07:40 -0500

    added post create for category

--- ObjectRelationalMapping/Develop/routes/api/category-routes.js
+++ ObjectRelationalMapping/Develop/routes/api/category-routes.js
@@ -25,9 +25,15 @@ router.get('/:id', async (req, res) => {
   return;
  }
 });
+//creating a  new category
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
