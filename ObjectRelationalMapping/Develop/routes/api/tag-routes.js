cf5308a82c27c369cba8ebd80e71a354e38c6d2d
Squiddy <wolaki96@gmail.com>
2022-11-09 22:27:59 -0500

    tag deletion

--- ObjectRelationalMapping/Develop/routes/api/tag-routes.js
+++ ObjectRelationalMapping/Develop/routes/api/tag-routes.js
@@ -51,9 +51,22 @@ router.put('/:id', async (req, res) => {
 }
 });
 
+// delete on tag by its id value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!tagData){
+      res.status(404).json({ message: 'No tag with this ID! Please try again.'});
+      return;
+    }
+    res.status(200).json(tagData);
+   }catch (err) {
+    res.status(500).json(err);
+   }
+  }
+);
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
 
 module.exports = router;
