0c474dc071da54b40518ec7070223c41b6dbebf4
Squiddy <wolaki96@gmail.com>
2022-11-09 19:38:28 -0500

    finished tag.js model set up

--- ObjectRelationalMapping/Develop/models/Tag.js
+++ ObjectRelationalMapping/Develop/models/Tag.js
@@ -6,8 +6,27 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
   },
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id'
+    }
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id'
+    }
+  }
+},
   {
     sequelize,
     timestamps: false,
