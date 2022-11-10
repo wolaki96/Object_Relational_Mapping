e05fd6c9c30a056b1b03e044a79b7a37393e8651
Squiddy <wolaki96@gmail.com>
2022-11-09 22:25:42 -0500

    added create a tag

--- ObjectRelationalMapping/Develop/routes/api/tag-routes.js
+++ ObjectRelationalMapping/Develop/routes/api/tag-routes.js
@@ -26,9 +26,15 @@ router.get('/:id', async (req, res) => {
 });
 
 //create a new tag
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
