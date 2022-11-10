e05c7f5e62a9d7a7c831729cc443d6b440a3f42a
Squiddy <wolaki96@gmail.com>
2022-11-09 22:32:33 -0500

    fixed typo from error. category_id to category

--- ObjectRelationalMapping/Develop/models/Product.js
+++ ObjectRelationalMapping/Develop/models/Product.js
@@ -37,7 +37,7 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category_id',
+        model: 'category',
         key: 'id',
         unique: false
       } 
