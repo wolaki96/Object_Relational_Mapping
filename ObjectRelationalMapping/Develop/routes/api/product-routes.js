83d51073584c09c14643d1b1d24b4f00aaa269e0
Squiddy <wolaki96@gmail.com>
2022-11-09 22:41:46 -0500

    fixed typos with models

--- ObjectRelationalMapping/Develop/routes/api/product-routes.js
+++ ObjectRelationalMapping/Develop/routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category, Tag}]
+      include: [{ model: Category}, { model: Tag}]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 // get one product by id
 router.get('/:id', async (req, res) => {
   const productData = await Product.findByPk(req.params.id, {
-    include: [{model: Category, Tag}]
+    include: [{model: Category},{model: Tag}]
   });
   if (!productData) {
     res.status(404).json({message: "No product found with this ID! Please try again."})
