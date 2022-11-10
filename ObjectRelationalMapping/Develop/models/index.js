a129d20e6c1c0853dcad4dd6a693d09951e2982c
Squiddy <wolaki96@gmail.com>
2022-11-09 19:13:44 -0500

    added belongToMany on index.js

--- ObjectRelationalMapping/Develop/models/index.js
+++ ObjectRelationalMapping/Develop/models/index.js
@@ -6,11 +6,40 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsToMany(Category, {
+  through:{
+    model: Tag,
+    unique: false
+  },
+  as: 'products_category'
+})
+
 // Categories have many Products
+Category.belongsToMany(Product, {
+  through:{
+    model: Tag,
+    unique: false
+  },
+  as: 'category_products'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through:{
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  through:{
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tagged_products'
+})
 
 module.exports = {
   Product,
