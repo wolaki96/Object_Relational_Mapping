492492a991acf2301c1ab88bb45c297b39afc3d0
Squiddy <wolaki96@gmail.com>
2022-11-09 22:24:53 -0500

    added find by all and id tags

--- ObjectRelationalMapping/Develop/routes/api/tag-routes.js
+++ ObjectRelationalMapping/Develop/routes/api/tag-routes.js
@@ -3,18 +3,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+});
+// single tag by its id
+router.get('/:id', async (req, res) => {
+  const tagData = await Tag.findByPk(req.params.id, {
+    include: [{ model: Product}]
+  });
+ if (!tagData) {
+  res.status(404).json({ message: "No tag found with this ID! Please try again."})
+  return;
+ }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
-
+//create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  
 });
 
 router.put('/:id', (req, res) => {
